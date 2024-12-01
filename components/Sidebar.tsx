e3ab307f904384e45d0fbf37092cba6b65075ae4
aessary <raressilviulazar@gmail.com>
2024-12-01 22:42:17 +0000

    Sidebar p1

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,31 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+        <Image
+          src="/assets/icons/logo-brand .svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6"></ul>
+      </nav>
+      Sidebar
+    </aside>
+  );
 };
 export default Sidebar;
