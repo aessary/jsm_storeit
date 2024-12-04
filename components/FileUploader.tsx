cd6448e50e2e3ec9d5d0e8981b56af71dae2e65c
aessary <raressilviulazar@gmail.com>
2024-12-04 15:54:20 +0000

    File Upload - Server Action & App Integration

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import React, { useCallback, useState } from "react";
-import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+
 import { useDropzone } from "react-dropzone";
 import { Button } from "@/components/ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
 import Image from "next/image";
-import Thumbnail from "./Thumbnail";
+import Thumbnail from "@/components/Thumbnail";
+import { MAX_FILE_SIZE } from "@/constants";
+import { useToast } from "@/hooks/use-toast";
+import { uploadFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 interface Props {
   ownerId: string;
@@ -13,15 +18,49 @@ interface Props {
   className?: string;
 }
 
-const FileUploader: React.FC<Props> = ({ ownerId, accountId, className }) => {
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const path = usePathname();
+  const { toast } = useToast();
   const [files, setFiles] = useState<File[]>([]);
 
-  const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    // Handle the dropped files
-    setFiles(acceptedFiles);
-  }, []);
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      setFiles(acceptedFiles);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+      const uploadPromises = acceptedFiles.map(async (file) => {
+        if (file.size > MAX_FILE_SIZE) {
+          setFiles((prevFiles) =>
+            prevFiles.filter((f) => f.name !== file.name),
+          );
+
+          return toast({
+            description: (
+              <p className="body-2 text-white">
+                <span className="font-semibold">{file.name}</span> is too large.
+                Max file size is 50MB.
+              </p>
+            ),
+            className: "error-toast",
+          });
+        }
+
+        return uploadFile({ file, ownerId, accountId, path }).then(
+          (uploadedFile) => {
+            if (uploadedFile) {
+              setFiles((prevFiles) =>
+                prevFiles.filter((f) => f.name !== file.name),
+              );
+            }
+          },
+        );
+      });
+
+      await Promise.all(uploadPromises);
+    },
+    [ownerId, accountId, path],
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   const handleRemoveFile = (
     e: React.MouseEvent<HTMLImageElement, MouseEvent>,
@@ -34,7 +73,7 @@ const FileUploader: React.FC<Props> = ({ ownerId, accountId, className }) => {
   return (
     <div {...getRootProps()} className="cursor-pointer">
       <input {...getInputProps()} />
-      <Button type="button" className={cn(`uploader-button`, className)}>
+      <Button type="button" className={cn("uploader-button", className)}>
         <Image
           src="/assets/icons/upload.svg"
           alt="upload"
@@ -46,8 +85,10 @@ const FileUploader: React.FC<Props> = ({ ownerId, accountId, className }) => {
       {files.length > 0 && (
         <ul className="uploader-preview-list">
           <h4 className="h4 text-light-100">Uploading</h4>
+
           {files.map((file, index) => {
             const { type, extension } = getFileType(file.name);
+
             return (
               <li
                 key={`${file.name}-${index}`}
@@ -65,15 +106,16 @@ const FileUploader: React.FC<Props> = ({ ownerId, accountId, className }) => {
                     <Image
                       src="/assets/icons/file-loader.gif"
                       width={80}
-                      height={80}
+                      height={26}
                       alt="Loader"
                     />
                   </div>
                 </div>
+
                 <Image
                   src="/assets/icons/remove.svg"
-                  height={24}
                   width={24}
+                  height={24}
                   alt="Remove"
                   onClick={(e) => handleRemoveFile(e, file.name)}
                 />
