789f5d07c3fc280ef2eeb9fc719b1ab9aebc66c9
aessary <raressilviulazar@gmail.com>
2024-12-04 17:56:35 +0000

    File Upload done

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,31 +3,37 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Search from "@/components/Search";
 import FileUploader from "@/components/FileUploader";
-import {signOutUser} from "@/lib/actions/user.actions";
+import { signOutUser } from "@/lib/actions/user.actions";
 
-const Header = () => {
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
-        <FileUploader />
-          <form
-              action={async () => {
-                  "use server";
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
 
-                  await signOutUser();
-              }}
-          >
-              <Button>
-                  <Image
-                      src="/assets/icons/logout.svg"
-                      alt="logo"
-                      width={24}
-                      height={24}
-                      className="w-6"
-                  />
-              </Button>
-          </form>
+            await signOutUser();
+          }}
+        >
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
       </div>
     </header>
   );
