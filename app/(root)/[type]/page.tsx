cf2585d6bc5b7c9c446317aaee8ff5361e0ceca1
aessary <raressilviulazar@gmail.com>
2024-12-05 11:13:49 +0000

    Dynamic Routing - File Display and Fetching

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import Sort from "@/components/ui/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
+  const files = await getFiles();
   return (
     <div className="page-container">
       <section className="w-full">
         <h1 className="h1 capitalize">{type}</h1>
+        <div className="total-size-section">
+          <p className="body-1">
+            Total size: <span className="h5">0 Mb</span>
+          </p>
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+            <Sort />
+          </div>
+        </div>
       </section>
+
+      {/* Render the files */}
+
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <h1 key={file.$id} className="h1">
+              {file.name}
+            </h1>
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
     </div>
   );
 };
