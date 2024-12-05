1fc9de1d0a617ea76000f62d5491f16b4f9a57f0
aessary <raressilviulazar@gmail.com>
2024-12-05 13:01:13 +0000

    File Card - Image Display

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,6 +3,7 @@ import { Models } from "node-appwrite";
 import Link from "next/link";
 import Thumbnail from "@/components/Thumbnail";
 import { convertFileSize } from "@/lib/utils";
+import FormattedDateTime from "@/components/FormattedDateTime";
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
@@ -20,8 +21,16 @@ const Card = ({ file }: { file: Models.Document }) => {
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
-
-      {file.name}
+      <div className="file-card-details">
+        <p className="subtitle-2 line-clamp-1">{file.name}</p>
+        <FormattedDateTime
+          date={file.$createdAt}
+          className="body-2 text-light-100"
+        />
+        <p className="caption line-clamp-1 text-light-200">
+          By: {file.owner.fullName}
+        </p>
+      </div>
     </Link>
   );
 };
